b63e890fa65883a12c4456f7296c55ad0bd78100
Hyefur Jonathan Balami <jonathanhyefur@gmail.com>
2022-08-04 17:05:05 +0100

    new change

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '95e9b8ce89fe16e0f0993829db8491dafcdcc3a8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '95e9b8ce89fe16e0f0993829db8491dafcdcc3a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
